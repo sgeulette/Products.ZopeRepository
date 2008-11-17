7dfa82a9dea2eb2ec5dce9c4bca5e785ab29bb67
Stephan Geulette <s.geulette@imio.be>
2008-11-17 14:30:31 +0000

    adapted log file

--- scripts/zope_repository_db.sh
+++ scripts/zope_repository_db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "## " `date +"%Y-%m-%d, %H:%M:%S"` " : Start of zope_repository_db" >>zr_db.log
+echo "## " `date +"%Y-%m-%d, %H:%M:%S"` " : Start of zope_repository_db"
 for i in `cat /root/zoperep_scripts/INSTANCES.txt`
-do /srv/python244/bin/python /root/zoperep_scripts/zope_repository_db.py -i $i >>zr_db.log 2>&1
+do /srv/python244/bin/python /root/zoperep_scripts/zope_repository_db.py -i $i
 done
-echo "## " `date +"%Y-%m-%d, %H:%M:%S"` " : End of zope_repository_db" >>zr_db.log
+echo "## " `date +"%Y-%m-%d, %H:%M:%S"` " : End of zope_repository_db"
