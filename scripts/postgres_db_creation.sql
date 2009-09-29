7a0c2b9f9439504222efd6974488cbc5d1204173
Stephan Geulette <s.geulette@imio.be>
2009-09-29 09:04:30 +0000

    added instance repository_address

--- scripts/postgres_db_creation.sql
+++ scripts/postgres_db_creation.sql
@@ -2,8 +2,6 @@
 -- PostgreSQL database dump
 --
 
--- Started on 2008-11-11 23:19:01 CET
-
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = off;
 SET check_function_bodies = false;
@@ -11,7 +9,6 @@ SET client_min_messages = warning;
 SET escape_string_warning = off;
 
 --
--- TOC entry 1811 (class 1262 OID 16467)
 -- Name: zoperepos; Type: DATABASE; Schema: -; Owner: zoperepos
 --
 
@@ -31,8 +28,6 @@ SET escape_string_warning = off;
 SET search_path = public, pg_catalog;
 
 --
--- TOC entry 1493 (class 1259 OID 16468)
--- Dependencies: 6
 -- Name: apaches_id_seq; Type: SEQUENCE; Schema: public; Owner: zoperepos
 --
 
@@ -51,8 +46,6 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- TOC entry 1494 (class 1259 OID 16470)
--- Dependencies: 1779 6
 -- Name: apaches; Type: TABLE; Schema: public; Owner: zoperepos; Tablespace: 
 --
 
@@ -67,8 +60,6 @@ CREATE TABLE apaches (
 ALTER TABLE public.apaches OWNER TO zoperepos;
 
 --
--- TOC entry 1495 (class 1259 OID 16474)
--- Dependencies: 6
 -- Name: instances_id_seq; Type: SEQUENCE; Schema: public; Owner: zoperepos
 --
 
@@ -82,8 +73,6 @@ CREATE SEQUENCE instances_id_seq
 ALTER TABLE public.instances_id_seq OWNER TO zoperepos;
 
 --
--- TOC entry 1496 (class 1259 OID 16476)
--- Dependencies: 1780 6
 -- Name: instances; Type: TABLE; Schema: public; Owner: zoperepos; Tablespace: 
 --
 
@@ -96,15 +85,14 @@ CREATE TABLE instances (
     plone_version character varying(30),
     type character(20),
     zope_path character(150),
-    server_id integer NOT NULL
+    server_id integer NOT NULL,
+    repository_address character varying(250)
 );
 
 
 ALTER TABLE public.instances OWNER TO zoperepos;
 
 --
--- TOC entry 1497 (class 1259 OID 16480)
--- Dependencies: 6
 -- Name: instances_products_id_seq; Type: SEQUENCE; Schema: public; Owner: zoperepos
 --
 
@@ -118,8 +106,6 @@ CREATE SEQUENCE instances_products_id_seq
 ALTER TABLE public.instances_products_id_seq OWNER TO zoperepos;
 
 --
--- TOC entry 1498 (class 1259 OID 16482)
--- Dependencies: 1781 6
 -- Name: instances_products; Type: TABLE; Schema: public; Owner: zoperepos; Tablespace: 
 --
 
@@ -140,8 +126,6 @@ CREATE TABLE instances_products (
 ALTER TABLE public.instances_products OWNER TO zoperepos;
 
 --
--- TOC entry 1499 (class 1259 OID 16489)
--- Dependencies: 6
 -- Name: mountpoints_id_seq; Type: SEQUENCE; Schema: public; Owner: zoperepos
 --
 
@@ -155,8 +139,6 @@ CREATE SEQUENCE mountpoints_id_seq
 ALTER TABLE public.mountpoints_id_seq OWNER TO zoperepos;
 
 --
--- TOC entry 1500 (class 1259 OID 16491)
--- Dependencies: 1782 6
 -- Name: mountpoints; Type: TABLE; Schema: public; Owner: zoperepos; Tablespace: 
 --
 
@@ -174,8 +156,6 @@ CREATE TABLE mountpoints (
 ALTER TABLE public.mountpoints OWNER TO zoperepos;
 
 --
--- TOC entry 1501 (class 1259 OID 16495)
--- Dependencies: 6
 -- Name: plonesites_id_seq; Type: SEQUENCE; Schema: public; Owner: zoperepos
 --
 
@@ -189,8 +169,6 @@ CREATE SEQUENCE plonesites_id_seq
 ALTER TABLE public.plonesites_id_seq OWNER TO zoperepos;
 
 --
--- TOC entry 1502 (class 1259 OID 16497)
--- Dependencies: 1783 6
 -- Name: plonesites; Type: TABLE; Schema: public; Owner: zoperepos; Tablespace: 
 --
 
@@ -206,8 +184,6 @@ CREATE TABLE plonesites (
 ALTER TABLE public.plonesites OWNER TO zoperepos;
 
 --
--- TOC entry 1503 (class 1259 OID 16501)
--- Dependencies: 6
 -- Name: plonesites_products_id_seq; Type: SEQUENCE; Schema: public; Owner: zoperepos
 --
 
@@ -221,8 +197,6 @@ CREATE SEQUENCE plonesites_products_id_seq
 ALTER TABLE public.plonesites_products_id_seq OWNER TO zoperepos;
 
 --
--- TOC entry 1504 (class 1259 OID 16503)
--- Dependencies: 1784 6
 -- Name: plonesites_products; Type: TABLE; Schema: public; Owner: zoperepos; Tablespace: 
 --
 
@@ -239,8 +213,6 @@ CREATE TABLE plonesites_products (
 ALTER TABLE public.plonesites_products OWNER TO zoperepos;
 
 --
--- TOC entry 1505 (class 1259 OID 16510)
--- Dependencies: 6
 -- Name: products_id_seq; Type: SEQUENCE; Schema: public; Owner: zoperepos
 --
 
@@ -254,8 +226,6 @@ CREATE SEQUENCE products_id_seq
 ALTER TABLE public.products_id_seq OWNER TO zoperepos;
 
 --
--- TOC entry 1506 (class 1259 OID 16512)
--- Dependencies: 1785 6
 -- Name: products; Type: TABLE; Schema: public; Owner: zoperepos; Tablespace: 
 --
 
@@ -268,8 +238,6 @@ CREATE TABLE products (
 ALTER TABLE public.products OWNER TO zoperepos;
 
 --
--- TOC entry 1507 (class 1259 OID 16516)
--- Dependencies: 6
 -- Name: rewrites_id_seq; Type: SEQUENCE; Schema: public; Owner: zoperepos
 --
 
@@ -284,8 +252,6 @@ CREATE SEQUENCE rewrites_id_seq
 ALTER TABLE public.rewrites_id_seq OWNER TO zoperepos;
 
 --
--- TOC entry 1508 (class 1259 OID 16518)
--- Dependencies: 1786 6
 -- Name: rewrites; Type: TABLE; Schema: public; Owner: zoperepos; Tablespace: 
 --
 
@@ -302,8 +268,6 @@ CREATE TABLE rewrites (
 ALTER TABLE public.rewrites OWNER TO zoperepos;
 
 --
--- TOC entry 1512 (class 1259 OID 16551)
--- Dependencies: 6
 -- Name: servers; Type: TABLE; Schema: public; Owner: zoperepos; Tablespace: 
 --
 
@@ -317,8 +281,26 @@ CREATE TABLE servers (
 ALTER TABLE public.servers OWNER TO zoperepos;
 
 --
--- TOC entry 1509 (class 1259 OID 16525)
--- Dependencies: 6
+-- Name: servers_id_seq; Type: SEQUENCE; Schema: public; Owner: zoperepos
+--
+
+CREATE SEQUENCE servers_id_seq
+    INCREMENT BY 1
+    NO MAXVALUE
+    NO MINVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.servers_id_seq OWNER TO zoperepos;
+
+--
+-- Name: servers_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: zoperepos
+--
+
+ALTER SEQUENCE servers_id_seq OWNED BY servers.id;
+
+
+--
 -- Name: virtualhosts_id_seq; Type: SEQUENCE; Schema: public; Owner: zoperepos
 --
 
@@ -333,8 +315,6 @@ CREATE SEQUENCE virtualhosts_id_seq
 ALTER TABLE public.virtualhosts_id_seq OWNER TO zoperepos;
 
 --
--- TOC entry 1510 (class 1259 OID 16527)
--- Dependencies: 1787 6
 -- Name: virtualhosts; Type: TABLE; Schema: public; Owner: zoperepos; Tablespace: 
 --
 
@@ -351,33 +331,6 @@ CREATE TABLE virtualhosts (
 ALTER TABLE public.virtualhosts OWNER TO zoperepos;
 
 --
--- TOC entry 1511 (class 1259 OID 16549)
--- Dependencies: 6 1512
--- Name: servers_id_seq; Type: SEQUENCE; Schema: public; Owner: zoperepos
---
-
-CREATE SEQUENCE servers_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MAXVALUE
-    NO MINVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.servers_id_seq OWNER TO zoperepos;
-
---
--- TOC entry 1814 (class 0 OID 0)
--- Dependencies: 1511
--- Name: servers_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: zoperepos
---
-
-ALTER SEQUENCE servers_id_seq OWNED BY servers.id;
-
-
---
--- TOC entry 1788 (class 2604 OID 16554)
--- Dependencies: 1511 1512 1512
 -- Name: id; Type: DEFAULT; Schema: public; Owner: zoperepos
 --
 
@@ -385,8 +338,6 @@ ALTER TABLE servers ALTER COLUMN id SET DEFAULT nextval('servers_id_seq'::regcla
 
 
 --
--- TOC entry 1790 (class 2606 OID 16532)
--- Dependencies: 1494 1494
 -- Name: apache_pk; Type: CONSTRAINT; Schema: public; Owner: zoperepos; Tablespace: 
 --
 
@@ -395,8 +346,6 @@ ALTER TABLE ONLY apaches
 
 
 --
--- TOC entry 1792 (class 2606 OID 16534)
--- Dependencies: 1496 1496
 -- Name: instance_pk; Type: CONSTRAINT; Schema: public; Owner: zoperepos; Tablespace: 
 --
 
@@ -405,8 +354,6 @@ ALTER TABLE ONLY instances
 
 
 --
--- TOC entry 1794 (class 2606 OID 16536)
--- Dependencies: 1498 1498
 -- Name: instance_product_pk; Type: CONSTRAINT; Schema: public; Owner: zoperepos; Tablespace: 
 --
 
@@ -415,8 +362,6 @@ ALTER TABLE ONLY instances_products
 
 
 --
--- TOC entry 1796 (class 2606 OID 16538)
--- Dependencies: 1500 1500
 -- Name: mountpoint_id; Type: CONSTRAINT; Schema: public; Owner: zoperepos; Tablespace: 
 --
 
@@ -425,8 +370,6 @@ ALTER TABLE ONLY mountpoints
 
 
 --
--- TOC entry 1798 (class 2606 OID 16540)
--- Dependencies: 1502 1502
 -- Name: plonesite_pk; Type: CONSTRAINT; Schema: public; Owner: zoperepos; Tablespace: 
 --
 
@@ -435,8 +378,6 @@ ALTER TABLE ONLY plonesites
 
 
 --
--- TOC entry 1800 (class 2606 OID 16542)
--- Dependencies: 1504 1504
 -- Name: plonesite_product_pk; Type: CONSTRAINT; Schema: public; Owner: zoperepos; Tablespace: 
 --
 
@@ -445,8 +386,6 @@ ALTER TABLE ONLY plonesites_products
 
 
 --
--- TOC entry 1802 (class 2606 OID 16544)
--- Dependencies: 1506 1506
 -- Name: product_pk; Type: CONSTRAINT; Schema: public; Owner: zoperepos; Tablespace: 
 --
 
@@ -455,8 +394,6 @@ ALTER TABLE ONLY products
 
 
 --
--- TOC entry 1804 (class 2606 OID 16546)
--- Dependencies: 1508 1508
 -- Name: rewrite_pk; Type: CONSTRAINT; Schema: public; Owner: zoperepos; Tablespace: 
 --
 
@@ -465,8 +402,6 @@ ALTER TABLE ONLY rewrites
 
 
 --
--- TOC entry 1808 (class 2606 OID 16556)
--- Dependencies: 1512 1512
 -- Name: servers_pkey; Type: CONSTRAINT; Schema: public; Owner: zoperepos; Tablespace: 
 --
 
@@ -475,8 +410,6 @@ ALTER TABLE ONLY servers
 
 
 --
--- TOC entry 1806 (class 2606 OID 16548)
--- Dependencies: 1510 1510
 -- Name: virtualhost_pk; Type: CONSTRAINT; Schema: public; Owner: zoperepos; Tablespace: 
 --
 
@@ -485,8 +418,6 @@ ALTER TABLE ONLY virtualhosts
 
 
 --
--- TOC entry 1813 (class 0 OID 0)
--- Dependencies: 6
 -- Name: public; Type: ACL; Schema: -; Owner: postgres
 --
 
@@ -495,8 +426,6 @@ REVOKE ALL ON SCHEMA public FROM postgres;
 GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
-
--- Completed on 2008-11-11 23:19:02 CET
 
 --
 -- PostgreSQL database dump complete
