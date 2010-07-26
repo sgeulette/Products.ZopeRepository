a1ee17b1f54e8542850f76235758ec26d0b97b35
André Nuyens <andre.nuyens@imio.be>
2010-07-26 11:26:25 +0000

    #1244 add test before remove log file

--- scripts/zope_repository_db2.sh
+++ scripts/zope_repository_db2.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-rm zr_db.log
+if [ -e zr_db.log ]; then
+   rm zr_db.log
+fi
 echo "## " `date +"%Y-%m-%d, %H:%M:%S"` " : Start of zope_repository_db" >>zr_db.log
 for i in `cat INSTANCES.txt`
-do /srv/python244/bin/python zope_repository_db.py -i $i >>zr_db.log 2>&1
+do /srv/python246/bin/python zope_repository_db.py -i $i >>zr_db.log 2>&1
 done
 echo "## " `date +"%Y-%m-%d, %H:%M:%S"` " : End of zope_repository_db" >>zr_db.log
