85c8884fa5ead4057394a441b5cd3038c22ad7f2
André Nuyens <andre.nuyens@imio.be>
2010-12-08 12:22:46 +0000

    include mini-site rewrite rules

--- scripts/apache_repository_db.py
+++ scripts/apache_repository_db.py
@@ -28,10 +28,12 @@ apaches = (
 pat_virtualhost     = re.compile(r"^<virtualhost +([\d\.:]+) *>",re.I)
 pat_virtualhost_end = re.compile(r"^</virtualhost",re.I)
 pat_servername      = re.compile(r'^servername +(.+)', re.I)
+pat_serveralias     = re.compile(r'^serveralias +(.+)', re.I)
 pat_log             = re.compile(r'^customlog +(\S+) *(combined)?', re.I)
 pat_serverroot      = re.compile(r'^serverroot +(?:\'|\")?(.+)(\'|\")', re.I)
 pat_rw_begin        = re.compile(r'^rewriterule +', re.I)
 pat_rw_other        = re.compile(r'^rewriterule +\^?/(stats|awstats|svn/|trac(/|\$))', re.I)
+pat_rw_map_mini     = re.compile(r'^rewritemap map-mini +(.+)', re.I)
 pat_rw_zope         = re.compile(r'^rewriterule +\^?/([\w-]*)(?:\(\.\*\)?) +http://localhost:(\d+)/VirtualHostBase/(https?)/([\w\-\.]+):(\d+)/([\w\-/]+)/VirtualHostRoot/', re.I)
 pat_redirect_begin  = re.compile(r'^redirect +', re.I)
 pat_redirect        = re.compile(r'^redirect +(\d+) +([\w\-/]+) +([\w\-/:\.]+)', re.I)
@@ -57,7 +59,8 @@ def main():
         deleteTable('virtualhosts')
         deleteTable('rewrites')
     row = selectOneInTable('servers', 'min(creationdate)')
-    if not row[0] or (now - row[0]) > timedelta(hours=5):    
+    if not row[0] or (now - row[0]) > timedelta(hours=5):   
+#    if True: 
         deleteTable('servers')    
 
     hostname = socket.gethostname()
@@ -79,7 +82,7 @@ def main():
             fileslist.sort()
             for filename in fileslist:
                 filepath = os.path.join(apache_dic['path'], filename)
-                if not os.path.isdir(filepath):
+                if not os.path.isdir(filepath):             
                     analyze_conf(filepath, apache_dic['name'], server_id)
 
     verbose("End of %s"%sys.argv[0])
@@ -106,9 +109,11 @@ def analyze_conf(conf_file, apache_name, server_id):
     except IOError:
         error("Cannot open %s file" % conf_file)
         return
-    vhost = servername = log_vh = redirect_code = redirect_from = redirect_to = servernameip = protocol = vh_ip = ''
+    vhost = servername = minisite = log_vh = redirect_code = redirect_from = redirect_to = servernameip = protocol = vh_ip = ''
+    serveralias = []
     rewrites = []
-    lnb = 0
+    maps_mini_dico = {}
+    lnb = 0    
     for line in cfile.readlines():
         lnb += 1
         line = line.strip(' \t\n')
@@ -130,6 +135,19 @@ def analyze_conf(conf_file, apache_name, server_id):
             servername = res.group(1)
             servernameip = getServernameIP(servername)
             trace("servername='%s'"%servername)
+            continue
+        
+        #ServerAlias
+        res = pat_serveralias.match(line)
+        if res:
+            serveralias.append(res.group(1))
+            trace("serveralias='%s'"%res.group(1))
+            continue        
+
+        #RewriteMap map-mini srv/minisites.d/filename.conf
+        res = pat_rw_map_mini.match(line) 
+        if res:
+            minisite = res.group(1)
             continue
 
         #ServerRoot "/usr/local/apache2OCSP"
@@ -156,16 +174,43 @@ def analyze_conf(conf_file, apache_name, server_id):
         #pat_rw_begin        = re.compile(r'^rewriterule +', re.I)
         #pat_rw_other        = re.compile(r'^rewriterule +\^?/(stats|awstats|svn/|trac(/|\$))', re.I)
         #pat_rw_zope         = re.compile(r'^rewriterule +\^?/([\w-]*)(?:\(\.\*\)?) +http://localhost:(\d+)/VirtualHostBase/(https?)/([\w\-\.]+):(\d+)/([\w\-/]+)/VirtualHostRoot/', re.I)
-        if pat_rw_begin.match(line):
+        if pat_rw_begin.match(line):            
             res = pat_rw_other.match(line)
             if res:
-                continue
-            res = pat_rw_zope.match(line)
-            if res:
-                rewrites.append((res.group(1), res.group(2), res.group(3), res.group(4), res.group(5), res.group(6)))
-                trace("folder='%s', zopeport='%s', protocol='%s', domain='%s', port='%s', path='%s'"%(res.group(1), res.group(2), res.group(3), res.group(4), res.group(5), res.group(6)))
-            else:
-                error("Matching missed in RewriteRule line %d:'%s'"%(lnb,line))
+                continue   
+            maps_mini_dico[servername] = '' #initialize main virtualhost definition
+            for sa in serveralias:
+                if sa.startswith('*.') or servername.find(sa)>=0:
+                    continue
+                maps_mini_dico[sa] = ''
+            if minisite:
+                try:
+                    minifile = open(minisite, 'r')
+                    for miniline in minifile.readlines():
+                        miniline = miniline.strip(' \n')
+                        if miniline and not miniline.startswith('#'):
+                            try:
+                                domain,path = miniline.split(' ')                            
+                                if maps_mini_dico.has_key(domain) and maps_mini_dico[domain]:
+                                    error("this key for minisite already exist and have a value")
+                                else:
+                                    maps_mini_dico[domain] = '/'+path
+                            except ValueError: 
+                                error("Bad formatting in minisite line %"%miniline)
+                except IOError:                
+                    error("Cannot open %s file" % minisite)
+            map_mini_keys = maps_mini_dico.keys()
+            for map_mini_key in map_mini_keys:   
+                adaptedline = line.replace('/${map-mini:%{SERVER_NAME}}',maps_mini_dico[map_mini_key])
+                adaptedline = adaptedline.replace('%{SERVER_NAME}',map_mini_key)
+                res = pat_rw_zope.match(adaptedline)
+                if res:
+                    rewrites.append((res.group(1), res.group(2), res.group(3), res.group(4), res.group(5), res.group(6)))
+                    trace("folder='%s', zopeport='%s', protocol='%s', domain='%s', port='%s', path='%s'"%(res.group(1), res.group(2), res.group(3), res.group(4), res.group(5), res.group(6)))
+                else:
+                    error("Matching missed in RewriteRule line %d:'%s'"%(lnb,line))
+                if line.find('%{SERVER_NAME}')<0:
+                    break
 
         #Redirect 301 / https://andenne.lescommunes.be
         #pat_redirect_begin  = re.compile(r'^redirect +', re.I)
