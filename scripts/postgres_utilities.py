07c62e5aea924a4bf13e78c283bdda827c5b0a0f
Stephan Geulette <s.geulette@imio.be>
2014-06-01 19:13:25 +0000

    Pep8 corrections. Added param to open connection

--- scripts/postgres_utilities.py
+++ scripts/postgres_utilities.py
@@ -5,22 +5,27 @@
 # Stéphan Geulette <stephan.geulette@uvcw.be>, UVCW
 #
 
-import sys, os
+import os
 import psycopg2
+
+TRACE = False
+
 
 def verbose(*messages):
     print '>>', ' '.join(messages)
+
+
 def error(*messages):
     print '!!', (' '.join(messages))
+
+
 def trace(*messages):
     if not TRACE:
         return
     print 'TRACE:', ' '.join(messages)
 
-TRACE = False
-
 #dsn="host=localhost port=5432 dbname=zoperepos user=zoperepos password=zopeREP1"
-dsn="host=localhost dbname=zoperepos user=zoperepos password=zopeREP1"
+dsn = "host=localhost dbname=zoperepos user=zoperepos password=zopeREP1"
 
 try:
     #The ZopeRepositoryLocalPassword product can be put in the buildout 'products' subdirectory
@@ -32,32 +37,33 @@ except (ImportError, NameError):
 
 #------------------------------------------------------------------------------
 
-def openConnection():
+
+def openConnection(dsn=dsn):
     """ open a postgres connection """
     conn = None
     try:
         #param :: dbname user password
-        conn=psycopg2.connect(dsn)
+        conn = psycopg2.connect(dsn)
     except Exception, message:
-        msg = "Cannot connect to database with dsn '%s': %s"%(dsn, message)
+        msg = "Cannot connect to database with dsn '%s': %s" % (dsn, message)
         error(msg)
         raise Exception(msg)
     return conn
 
 #------------------------------------------------------------------------------
 
+
 def insertInTable(table, columns, vals):
     """ insert values in a table """
     conn = openConnection()
     cursor = conn.cursor()
-    req="insert into %s(%s) \
-                values(%s)"%(table, columns, vals)
+    req = "insert into %s(%s) values(%s)" % (table, columns, vals)
 #    verbose("Insertion : %s"%req)
     try:
         cursor.execute(req)
     except Exception, message:
-        error("Cannot insert in database : %s"%message)
-        error("Request was : '%s'"%req)
+        error("Cannot insert in database : %s" % message)
+        error("Request was : '%s'" % req)
         conn.close()
         return False
     conn.commit()
@@ -65,20 +71,21 @@ def insertInTable(table, columns, vals):
     return True
 
 #------------------------------------------------------------------------------
+
 
 def updateTable(table, updates, condition=''):
     """ update columns in a table """
     conn = openConnection()
     cursor = conn.cursor()
-    req="update %s set %s"%(table, updates)
+    req = "update %s set %s" % (table, updates)
     if condition:
-        req += ' where %s'%condition
+        req += ' where %s' % condition
 #    verbose("Update : %s"%req)
     try:
         cursor.execute(req)
     except Exception, message:
-        error("Cannot update in database : %s"%message)
-        error("Request was : '%s'"%req)
+        error("Cannot update in database : %s" % message)
+        error("Request was : '%s'" % req)
         conn.close()
         return False
     conn.commit()
@@ -86,60 +93,63 @@ def updateTable(table, updates, condition=''):
     return True
 
 #------------------------------------------------------------------------------
+
 
 def selectWithSQLRequest(sql):
     """ select multiple lines in a table with a complete sql"""
     conn = openConnection()
     cursor = conn.cursor()
-    req=sql
+    req = sql
     try:
         cursor.execute(req)
         data = cursor.fetchall()
     except Exception, message:
-        error("Cannot select from database : %s"%message)
-        error("Request was : '%s'"%req)
+        error("Cannot select from database : %s" % message)
+        error("Request was : '%s'" % req)
         conn.close()
         return None
     conn.close()
     return data
 
 #------------------------------------------------------------------------------
+
 
 def selectAllInTable(table, selection, condition=''):
     """ select multiple lines in a table """
     conn = openConnection()
     cursor = conn.cursor()
-    req="select %s from %s"%(selection, table)
+    req = "select %s from %s" % (selection, table)
     if condition:
-        req += ' where %s'%condition
+        req += ' where %s' % condition
 #    verbose("Selection : %s"%req)
     try:
         cursor.execute(req)
         data = cursor.fetchall()
     except Exception, message:
-        error("Cannot select from database : %s"%message)
-        error("Request was : '%s'"%req)
+        error("Cannot select from database : %s" % message)
+        error("Request was : '%s'" % req)
         conn.close()
         return None
     conn.close()
     return data
 
 #------------------------------------------------------------------------------
+
 
 def selectOneInTable(table, selection, condition=''):
     """ select a single line in a table """
     conn = openConnection()
     cursor = conn.cursor()
-    req="select %s from %s"%(selection, table)
+    req = "select %s from %s" % (selection, table)
     if condition:
-        req += ' where %s'%condition
+        req += ' where %s' % condition
 #    verbose("Selection : %s"%req)
     try:
         cursor.execute(req)
         data = cursor.fetchone()
     except Exception, message:
-        error("Cannot select from database : %s"%message)
-        error("Request was : '%s'"%req)
+        error("Cannot select from database : %s" % message)
+        error("Request was : '%s'" % req)
         conn.close()
         return None
     conn.close()
@@ -147,19 +157,20 @@ def selectOneInTable(table, selection, condition=''):
 
 #------------------------------------------------------------------------------
 
+
 def deleteTable(table, condition=''):
     """ delete a table """
     conn = openConnection()
     cursor = conn.cursor()
-    req="delete from %s"%(table)
+    req = "delete from %s" % (table)
     if condition:
-        req += ' where %s'%condition
-    trace("Deletion : %s"%req)
+        req += ' where %s' % condition
+    trace("Deletion : %s" % req)
     try:
         cursor.execute(req)
     except Exception, message:
-        error("Cannot delete from database : %s"%message)
-        error("Request was : '%s'"%req)
+        error("Cannot delete from database : %s" % message)
+        error("Request was : '%s'" % req)
         conn.close()
         return False
     conn.commit()
@@ -168,13 +179,14 @@ def deleteTable(table, condition=''):
 
 #------------------------------------------------------------------------------
 
+
 def runCommand(cmd):
     """ run an os command and get back the stdout and stderr outputs """
-    ret = os.system(cmd + ' >_cmd_pv.out 2>_cmd_pv.err')
+    os.system(cmd + ' >_cmd_pv.out 2>_cmd_pv.err')
     stdout = stderr = []
     try:
         if os.path.exists('_cmd_pv.out'):
-            ofile = open( '_cmd_pv.out', 'r')
+            ofile = open('_cmd_pv.out', 'r')
             stdout = ofile.readlines()
             ofile.close()
             os.remove('_cmd_pv.out')
@@ -184,7 +196,7 @@ def runCommand(cmd):
         error("Cannot open %s file" % '_cmd_pv.out')
     try:
         if os.path.exists('_cmd_pv.err'):
-            ifile = open( '_cmd_pv.err', 'r')
+            ifile = open('_cmd_pv.err', 'r')
             stderr = ifile.readlines()
             ifile.close()
             os.remove('_cmd_pv.err')
@@ -192,18 +204,6 @@ def runCommand(cmd):
             error("File %s does not exist" % '_cmd_pv.err')
     except IOError:
         error("Cannot open %s file" % '_cmd_pv.err')
-    return( stdout,stderr )
-
-#------------------------------------------------------------------------------
-
-#problem with big output : subprocess is frozen
-def runCommand2(cmd):
-    (child_stdin, child_stdout, child_stderr) = os.popen3(cmd, 't', 1000000)
-    stderr = child_stderr.readlines()
-    child_stderr.close()
-    stdout = child_stdout.readlines()
-    child_stdout.close()
-    child_stdin.close()
-    return( stdout,stderr )
+    return(stdout, stderr)
 
 #------------------------------------------------------------------------------
